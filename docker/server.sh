35fd8629d04bbba92dcff9ffb95a8bceeadfbfbf
charles hayden <cchayden@gmail.com>
2017-05-22 15:52:20 -0700

    No longer needed

--- docker/server.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-
-cd /base/docs && make html
-cp -r _build/html/* /usr/share/nginx/html
-service nginx start
-
-cd /notebooks
-ipython notebook --no-browser --port 8888 --ip=* --matplotlib=inline
